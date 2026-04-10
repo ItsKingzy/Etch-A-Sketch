1229deb0837bff8af70579f82d95a605ac868509
Cameron L. <imcelarson@gmail.com>
2026-04-09 23:29:32 -0600

    Redid logic for cell creation
    purpose: later I can manipulate this algo to allow for nxn grid size

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Create a 16 x 16 grid of square divs
 const grid = document.querySelector("#grid");
 
+let n = 16;  // Length of one axis on the grid
 let gridSize = 16*16
-for (let i = 0; i < gridSize; i++) {
-    let cell = document.createElement("div");
-    grid.appendChild(cell);
-}   
+
+// Setup number of rows in the grid
+for (let i = 0; i < n; i++) {
+    let row = document.createElement("div");
+    row.id = "row" + i;
+    grid.appendChild(row);
+}
+
+// For each row, append n cells
+for (let i = 0; i < n; i++) {
+    let row = document.querySelector("#row" + i);
+    for (let j = 0; j < n; j++) {
+        let cell = document.createElement("div");
+        row.appendChild(cell);
+    }
+}

ccd7a4bc3852a0119cbf492c249a8a63ce8f3b9e
Cameron L. <imcelarson@gmail.com>
2026-04-12 23:35:47 -0600

    Made function for setting grid size and added dynamic input range (slider)

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 // Create a 16 x 16 grid of square divs
 const grid = document.querySelector("#grid");
 
-let n = 16;  // Length of one axis on the grid
-let gridSize = 16*16
+function updateGridSize(size) {
+    // Setup number of rows in the grid
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("col");
+        row.classList.add("col" + i);
+        grid.appendChild(row);
+    }
 
-// Setup number of rows in the grid
-for (let i = 0; i < n; i++) {
-    let row = document.createElement("div");
-    row.classList.add("col");
-    row.classList.add("col" + i);
-    grid.appendChild(row);
-}
+    // Setup number of rows in the grid
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("col");
+        row.classList.add("col" + i);
+        grid.appendChild(row);
+    }
 
-// For each row, append n cells
-for (let i = 0; i < n; i++) {
-    let row = document.querySelector(".col.col" + i);
-    for (let j = 0; j < n; j++) {
-        let cell = document.createElement("div");
-        row.appendChild(cell);
+    // For each row, append n cells
+    for (let i = 0; i < size; i++) {
+        let row = document.querySelector(".col.col" + i);
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement("div");
+            row.appendChild(cell);
+        }
     }
 }
 
+updateGridSize(16); // Set default grid size to 16
 
 
 /* Hover effect on hover of 1 cell */
 let cell = document.querySelectorAll(".col div");
 // Hover effect
 cell.forEach(div => {
-   div.addEventListener("mouseover", (event) => {
+    div.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "white";
     }); 
 });
